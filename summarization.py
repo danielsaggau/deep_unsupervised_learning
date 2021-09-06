fe4c8b49b8912d4b839dd13f4ba867c9c288d61e
Daniel-Philip Saggau <daniel.saggau@campus.lmu.de>
2021-09-07 01:10:58 +0200

    summarization structure

---
+++ summarization.py
@@ -0,0 +1,12 @@
+import transformers
+import pytorch
+
+# load data and format correctly / either medical text summarization or scientific text summarization / german
+
+# instantiate tokenizer / encoding
+
+# instantiate dataset
+
+
+# decoding strategy / reweight decoding
+
